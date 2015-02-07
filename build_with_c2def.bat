c2fee9c58940e4771c1048cd2b6fa4beaa43c0bb
dck-jp <dicek.am@clockahead.com>
2015-02-08 01:14:59 +0900

    change ProjectName to variable @build_with_c2def.bat
    & L13: change call myself (bat filename) to %0

--- build_with_c2def.bat
+++ build_with_c2def.bat
@@ -1,12 +1,13 @@
 @echo off
 pushd %~dp0
+SET ProjectName="c2def"
 SET FrameworkPath="%Systemroot%\Microsoft.NET\Framework\"
 SET PATH="%PATH%;%FrameworkPath%v4.0.30319\;%FrameworkPath%v3.5;%FrameworkPath%v3.0;"
-csc /out:c2def.exe c2def\*.cs
+csc /out:%ProjectName%.exe %ProjectName%\*.cs
 if %ERRORLEVEL% NEQ 0 goto FAILURE
 c2def
 cscript //nologo vbac.wsf combine
 exit
 
 :FAILURE
-build_with_c2def.bat
+%0
