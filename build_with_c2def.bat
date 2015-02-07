55f1244057dd095a5ec00057c64f3768b8a5a804
dck-jp <dicek.am@clockahead.com>
2015-02-08 01:10:22 +0900

    add error trap @ build_with_c2def.bat
    able to execute csript at 1st time

--- build_with_c2def.bat
+++ build_with_c2def.bat
@@ -1,10 +1,12 @@
-REM cscriptが見つからない。みたいなエラーが出た場合、
-REM コマンドプロンプトで２回呼び出すと実行できる。（再現性不明）
-
 @echo off
 pushd %~dp0
 SET FrameworkPath="%Systemroot%\Microsoft.NET\Framework\"
 SET PATH="%PATH%;%FrameworkPath%v4.0.30319\;%FrameworkPath%v3.5;%FrameworkPath%v3.0;"
 csc /out:c2def.exe c2def\*.cs
+if %ERRORLEVEL% NEQ 0 goto FAILURE
 c2def
 cscript //nologo vbac.wsf combine
+exit
+
+:FAILURE
+build_with_c2def.bat
