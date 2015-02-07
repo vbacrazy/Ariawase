832668d94d724b3c9a92271a8fe79917431d1124
dck-jp <dicek.am@clockahead.com>
2015-02-08 01:35:05 +0900

    FrameworkPath get by registry

--- build_with_c2def.bat
+++ build_with_c2def.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd %~dp0
 SET ProjectName="c2def"
-SET FrameworkPath="%Systemroot%\Microsoft.NET\Framework\"
+FOR /F "TOKENS=1,2,*" %%I IN ('REG QUERY "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework" /v "InstallRoot"') DO IF "%%I"=="InstallRoot" SET FrameworkPath=%%K
 SET PATH="%PATH%;%FrameworkPath%v4.0.30319\;%FrameworkPath%v3.5;%FrameworkPath%v3.0;"
 csc /out:%ProjectName%.exe %ProjectName%\*.cs
 if %ERRORLEVEL% NEQ 0 goto FAILURE
