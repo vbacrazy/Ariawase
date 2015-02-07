ca84cb0eb8fc12a8b3b11a355ebb862bff0275e1
D*isuke YAMAKAWA <dicek.am@clockahead.com>
2015-02-07 09:26:08 +0900

    add c2def
    concept model:
    convert the encoding of source files to the default encoding before
    combine, by using c2def

---
+++ build_with_c2def.bat
@@ -0,0 +1,10 @@
+REM cscriptが見つからない。みたいなエラーが出た場合、
+REM コマンドプロンプトで２回呼び出すと実行できる。（再現性不明）
+
+@echo off
+cd %~dp0
+SET FrameworkPath="%Systemroot%\Microsoft.NET\Framework\"
+SET PATH="%PATH%;%FrameworkPath%v4.0.30319\;%FrameworkPath%v3.5;%FrameworkPath%v3.0;"
+csc /out:c2def.exe c2def\*.cs
+c2def
+cscript //nologo vbac.wsf combine
